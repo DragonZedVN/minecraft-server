1e0f368dd690db7c07fed17fb747b5aa66104799
Đỗ Văn Hoài Tuân <77253698+AutumnVN@users.noreply.github.com>
2021-06-27 21:09:04 +0700

    ip

--- run-mc.sh
+++ run-mc.sh
@@ -1,5 +1,5 @@
 cd server
 . ../setup-auto-mc.cfg
 echo "eula=true">eula.txt
-echo "6969420"
+echo "SERVER IP: $(curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url)"
 java -Xmx8G -Xms8G -jar "$jar" nogui
