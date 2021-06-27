83b57debf55261a58749b119665a650a824e0450
Đỗ Văn Hoài Tuân <77253698+AutumnVN@users.noreply.github.com>
2021-06-27 20:49:33 +0700

    .

--- setup-ngrok.sh
+++ setup-ngrok.sh
@@ -2,5 +2,4 @@ wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip ngrok-stable-linux-amd64.zip
 chmod +x ngrok
 ./ngrok authtoken $ngrokauthtoken
-./ngrok tcp 25565
-echo "$(curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url)"
+./ngrok tcp --region ap 25565
